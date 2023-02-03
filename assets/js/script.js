45cd3b85b7bb847f61dafae584dd89257836e557
heli-sa <heli.saarts@gmail.com>
2023-02-03 19:33:49 +0000

    Add buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,26 @@ function startGame(){
 console.log('Started')
 }
 
+
+
 function handleQuestions() 
+
+
+
+
+
+function nextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("q-number").innerHTML = questionNumber
+    document.getElementById("p-score").innerHTML = playerScore
+    document.getElementById("question").innerHTML = currentQuestion.question;
+    document.getElementById("one").innerHTML = currentQuestion.optionA;
+    document.getElementById("two").innerHTML = currentQuestion.optionB;
+    document.getElementById("three").innerHTML = currentQuestion.optionC;
+    document.getElementById("four").innerHTML = currentQuestion.optionD;
+
+}
 
 
 function nextQuestion()
