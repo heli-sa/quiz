e14500b023b38f5eb3357145d8e4ca99bf1b5d7a
heli-sa <heli.saarts@gmail.com>
2023-02-03 17:56:59 +0000

    Add functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,27 @@ const startButton =  document.getElementById('play-btn')
 function startGame(){
 console.log('Started')
 }
+
+function handleQuestions() 
+
+
+function nextQuestion()
+
+
+function checkAnswer()
+
+
+
+function takeNextQuestion()
+
+function resetOption()
+
+
+function optionButton()
+
+function endGame()
+
+
+function closeScore()
+
+function closeOption()
