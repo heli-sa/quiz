155fa72d2a56cd4a063f89169fa6e8d8b16264f8
heli-sa <heli.saarts@gmail.com>
2023-02-04 12:49:06 +0000

    add script.js with initial question array

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// Notes on functions and variables needed
+// Javascript
+// Variables
+// Array to store questions
+// Timer, Username
+
+// Functions
+// Load questions
+// GetUserName
+// Display first question
+// Randomisation of questions (on/off)
+// Start Quiz
+// Start Timer
+// Stop Timer
+// Timer Set Time for Quiz
+// GotoNextQuestion
+// ShowAnswer
+// Listener for Start Button
+// Listener for Next Button
+// Create LeaderBoard
+// Update LeaderBoard
+// Clear LeaderBoard
+
+//store question text, options and answers in an array
+// Note: put option in code to read this array from JSON file (time permitting)
+const questions = [
+    {
+      questionText: "Question 1",
+      options: ["1. ans1", "2. ans2", "3. ans3", "4. ans4"],
+      answer: "3. ans3",
+    },
+    {
+      questionText: "Question 2",
+      options: ["1. ans1", "2. ans2", "3. ans3", "4. ans4"],
+      answer: "4. ans4",
+    },
+    {
+        questionText: "Question 3",
+        options: ["1. ans1", "2. ans2", "3. ans3", "4. ans4"],
+        answer: "2. ans2",
+    },
+];
